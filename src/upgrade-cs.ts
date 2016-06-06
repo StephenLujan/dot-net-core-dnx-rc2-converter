f7a2b044a0b6be2ff288e425d1aade281b0a598c
Stephen Lujan <StephenLujan@gmail.com>
2016-06-06 12:50:13 -0400

    fixed: the regular expressions in upgrade-cs.ts weren't matching

--- src/upgrade-cs.ts
+++ src/upgrade-cs.ts
@@ -1,7 +1,7 @@
 export module UpgradeCs {
     export function upgrade(input:string):string {
-        let output = input.replace(/^using Microsoft\.AspNet\./g, 'using Microsoft.AspNetCore.');
-        output = output.replace(/^using Microsoft\.Data\.Entity/g, 'using Microsoft.EntityFrameworkCore');
+        let output = input.replace(/using Microsoft\.AspNet\./g, 'using Microsoft.AspNetCore.');
+        output = output.replace(/using Microsoft\.Data\.Entity/g, 'using Microsoft.EntityFrameworkCore');
         return output;
     }
 }

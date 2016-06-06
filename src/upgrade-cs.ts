b3aa2fd0f62b2a234498ed67785dd14a7b1b926d
Stephen Lujan <StephenLujan@gmail.com>
2016-06-06 14:12:46 -0400

    added some more *.cs code changes

--- src/upgrade-cs.ts
+++ src/upgrade-cs.ts
@@ -1,7 +1,25 @@
+
+
 export module UpgradeCs {
+   function replaceAll(target: string, search: string, replacement: string): string {
+        return target.split(search).join(replacement);
+    };
+
+    const STRING_REPLACEMENTS = {
+        'using Microsoft.AspNet.': 'using Microsoft.AspNetCore.',
+        'using Microsoft.Data.Entity': 'using Microsoft.EntityFrameworkCore',
+        'using Microsoft.AspNetCore.Identity.EntityFramework': 'using Microsoft.AspNetCore.Identity.EntityFrameworkCore',
+        'Microsoft.Data.Entity.Metadata': 'Microsoft.EntityFrameworkCore.Metadata',
+        'Microsoft.Data.Entity.Migrations': 'Microsoft.EntityFrameworkCore.Migrations',
+        'Microsoft.Data.Entity.Infrastructure': 'Microsoft.EntityFrameworkCore.Infrastructure',
+        'HttpNotFound': 'NotFound',
+        'HttpBadRequest': 'BadRequest'
+    };
+
     export function upgrade(input:string):string {
-        let output = input.replace(/using Microsoft\.AspNet\./g, 'using Microsoft.AspNetCore.');
-        output = output.replace(/using Microsoft\.Data\.Entity/g, 'using Microsoft.EntityFrameworkCore');
-        return output;
+        for (let change in STRING_REPLACEMENTS) {
+            input = replaceAll(input, change, STRING_REPLACEMENTS[change]);
+        }
+        return input;
     }
 }

6d4fe0a5c6f5dd5793100960cb0bff8b5d9136ae
Stephen Lujan <StephenLujan@gmail.com>
2016-06-10 15:37:43 -0400

    fixed upgrade-project-json/frameworks.ts to keep framework specific dependencies and frameworkAssemblies while upgrading from dnx to their new equivalents

--- src/upgrade-project-json/frameworks.ts
+++ src/upgrade-project-json/frameworks.ts
@@ -13,41 +13,53 @@ const FRAMEWORK_NETCOREAPP = {
         }
     }
 };
-
+const FRAMEWORK_NET46 = {};
 const FRAMEWORK_NET452 = {
-    "imports": [
-        "dnx45",
-        "dnx451",
-        "dnx452"
-    ]
+    //"imports": [
+    //    "dnx45",
+    //    "dnx451",
+    //    "dnx452"
+    //]
 };
+const FRAMEWORK_NET45 = {};
 
 const FRAMEWORKS = {
-    //"netcoreapp1.0": FRAMEWORK_NETCOREAPP
-    //"net46": {},
-    "net452": FRAMEWORK_NET452
+    "netcoreapp1.0": FRAMEWORK_NETCOREAPP,
+    "net46": FRAMEWORK_NET46,
+    "net452": FRAMEWORK_NET452,
+    "net45": FRAMEWORK_NET45
+};
+
+const OLD_TO_NEW_FRAMEWORK_MAP = {
+    "dnx451": "net452",
+    "dnx45": "net45",
+    "dnx46 ": "net46",
+    "dnxcore50": "netcoreapp1.0"
 };
 
 export function upgrade(object:{dependencies:{}, frameworks:{}}):void {
     // move dependencies out of old frameworks
-    let frameworkAssemblies = {};
+
+    let newFrameworks = {};
     for (let frameworkName in object.frameworks) {
-        if (frameworkName.includes('dnx')) {
+        if (frameworkName in OLD_TO_NEW_FRAMEWORK_MAP) {
             let framework = object.frameworks[frameworkName];
+            let newFrameworkName = OLD_TO_NEW_FRAMEWORK_MAP[frameworkName];
+            let newFramework = FRAMEWORKS[newFrameworkName];
             if (framework['dependencies']) {
-                if (!object.dependencies) {
-                    object.dependencies = {};
-                }
-                Object.assign(object.dependencies, (framework['dependencies']));
+                newFramework.dependencies = Object.assign(
+                    newFramework.dependencies || {}, (framework['dependencies']));
             }
             if (framework['frameworkAssemblies']) {
-                Object.assign(frameworkAssemblies, framework['frameworkAssemblies']);
-                console.log(`Stray framework assemblies ${
-                    JSON.stringify(framework['frameworkAssemblies'])}`);
+                newFramework.frameworkAssemblies = Object.assign(
+                    newFramework.frameworkAssemblies || {}, framework['frameworkAssemblies']);
+            }
+            newFrameworks[newFrameworkName]= newFramework;
+        } else {
+            if (!frameworkName.startsWith('net')) {
+                throw new Error(`unmapped framework used: ${frameworkName}`);
             }
         }
     }
-
-    // update the frameworks section
-    object.frameworks = FRAMEWORKS;
+    object.frameworks = newFrameworks;
 }
